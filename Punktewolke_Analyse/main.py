79778e6c9a2b6e05729708d54827369241aa72e0
allmyusernamesaretaken <Max.Reisch@outlook.com>
2024-01-22 21:57:17 +0100

    Update main.py

--- Punktewolke_Analyse/main.py
+++ Punktewolke_Analyse/main.py
@@ -3,6 +3,11 @@ import tof_pothole_analysis as tpa
 import argparse
 
 
+def save_text(text, output_path):
+    with open(output_path, "a") as file:
+        file.write(text)
+
+
 def main():
     """"""
 
@@ -12,9 +17,10 @@ def main():
     parser.add_argument("--p2x", type=float, required=True, help="point2x")
     parser.add_argument("--p2y", type=float, required=True, help="point2y")
     parser.add_argument("--dir", default="", type=str, help="directory")
-    parser.add_argument("--create_rrf", default="True", type=str, help="create rrf or use existing one")
+    parser.add_argument("--counter", type=int, required=True, help="counter")
+    parser.add_argument("--create_rrf", action="store_true", default=False, help="create rrf or use existing one")
     parser.add_argument("--angle", default=90, type=float, help="angle of camera")
-    options = parser.parse_args()
+    options, _ = parser.parse_known_args()
 
     print(options.p1x)
     print(options.p1y)
@@ -24,9 +30,13 @@ def main():
     print(options.create_rrf)
 
     tofAnalysis = tpa.TofPotholeAnalysis([(options.p1x, options.p1y)], [(options.p2x, options.p2y)],
-                                         45, "samples", rrfFile="samples/recording.rrf")
+                                         45, options.dir, rrfFile="samples/recording.rrf")
     tof_potholes = tofAnalysis.get_potholes()
     for pothole in tof_potholes:
+        save_text(f"Average Pothole Depth: {pothole.get_avg_pothole_depth()}\n",
+                  f"data/results/{options.counter}/result.txt")
+        save_text(f"Max Pothole Depth: {pothole.get_max_pothole_depth()}\n",
+                  f"data/results/{options.counter}/result.txt")
         print("Maximale Tiefe: " + str(pothole.get_max_pothole_depth() * 10 ** 3) + "mm")
         print("durchschnittliche Tiefe: " + str(pothole.get_avg_pothole_depth() * 10 ** 3) + "mm")
 
